600b184e9dfb61d446e65b252cc1d18472671e93
Yana23Arina <gruschinayana@yandex.ru>
2026-03-31 00:32:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// server.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { importDump } = require('./importDump'); // на случай ручного импорта
 
 dotenv.config();
 
@@ -11,19 +13,23 @@ const adminRoutes = require('./routes/admin');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Мидлвары
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Роуты
 app.use('/api/auth', authRoutes);
 app.use('/api/museum', museumRoutes);
 app.use('/api/admin', adminRoutes);
 
+// Глобальная обработка ошибок
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Что-то пошло не так!' });
 });
 
+// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
